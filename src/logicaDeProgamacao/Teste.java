70f1285ce818dc4bce8e038e9a99893e2ab5b197
pedrormedeiros <perodriigues39@gmail.com>
2024-01-24 09:35:08 -0300

    Contrustores e atributos

--- src/logicaDeProgamacao/Teste.java
+++ src/logicaDeProgamacao/Teste.java
@@ -3,7 +3,6 @@ package logicaDeProgamacao;
 public class Teste {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
---
+++ src/pooTurma/Turma.java
@@ -0,0 +1,30 @@
+package pooTurma;
+
+public class Turma {
+    //Atributos da classe
+	String nome;
+	String telefone;
+	String email;
+	
+	//Construtor
+	public Turma() {
+		System.out.println("_______________");
+		
+	}
+		
+	//Métodos
+	void ligarPc() {
+		System.out.println("Computador ligado");
+		
+	}
+	
+	void logar() {
+		System.out.println("Login feito com sucesso");
+		
+	}
+	
+	void desligarPc() {
+		System.out.println("Computador desligado");
+		
+	}
+}      
